f4ed2b64b54626f1d09bfa495819f7292a2701be
prashantpandey1 <prashantpandey2709@gmail.com>
2021-05-30 00:23:49 +0530

    sign in validation with testcases done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
+  validates :email, presence: true, length: {maximum: 255},
+            format: {with: VALID_EMAIL_REGEX },
+            uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
 end
